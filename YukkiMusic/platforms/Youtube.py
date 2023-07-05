216513d052c89721b2daea8911c4365e1b4da07c
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-06 01:22:46 +0700

    Update Youtube.py

--- YukkiMusic/platforms/Youtube.py
+++ YukkiMusic/platforms/Youtube.py
@@ -43,7 +43,7 @@ async def shell_cmd(cmd):
 class YouTubeAPI:
     def __init__(self):
         self.base = "https://www.youtube.com/watch?v="
-        self.base = "https://fb.watch/"
+        self.base = "https://www.facebook.com/watch/?v="
         self.regex = r"(?:youtube\.com|youtu\.be)"
         self.status = "https://www.youtube.com/oembed?url="
         self.listbase = "https://youtube.com/playlist?list="
