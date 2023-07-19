ce686c199df95af959006fbb4c0a9afdde3cd29b
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-19 18:36:33 +0700

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -7,169 +7,168 @@
 #
 # All rights reserved.
 
-HELP_1 = """✅**<u>Admin Commands:</u>**
+HELP_1 = """✅**<u>Lệnh quản trị:</u>**
 
-**c** stands for channel play.
+**c** là viết tắt của phát kênh.
 
-/pause or /cpause - Pause the playing music.
-/resume or /cresume- Resume the paused music.
-/mute or /cmute- Mute the playing music.
-/unmute or /cunmute- Unmute the muted music.
-/skip or /cskip- Skip the current playing music.
-/stop or /cstop- Stop the playing music.
-/shuffle or /cshuffle- Randomly shuffles the queued playlist.
-/seek or /cseek - Forward Seek the music to your duration
-/seekback or /cseekback - Backward Seek the music to your duration
-/restart - Restart bot for your chat .
-
-
-✅<u>**Specific Skip:**</u>
-/skip or /cskip [Number(example: 3)] 
-    - Skips music to a the specified queued number. Example: /skip 3 will skip music to third queued music and will ignore 1 and 2 music in queue.
-
-✅<u>**Loop Play:**</u>
-/loop or /cloop [enable/disable] or [Numbers between 1-10] 
-    - When activated, bot loops the current playing music to 1-10 times on voice chat. Default to 10 times.
-
-✅<u>**Auth Users:**</u>
-Auth Users can use admin commands without admin rights in your chat.
-
-/auth [Username] - Add a user to AUTH LIST of the group.
-/unauth [Username] - Remove a user from AUTH LIST of the group.
-/authusers - Check AUTH LIST of the group."""
+/pause hoặc /cpause - Tạm dừng nhạc đang phát.
+/resume hoặc /cresume- Tiếp tục nhạc bị tạm dừng.
+/mute hoặc /cmute- Tắt tiếng nhạc đang phát.
+/bật tiếng hoặc /cunmute- Bật tiếng nhạc bị tắt tiếng.
+/skip hoặc /cskip- Bỏ qua nhạc đang phát.
+/stop hoặc /cstop- Dừng phát nhạc.
+/shuffle hoặc /cshuffle- Xáo trộn ngẫu nhiên danh sách phát đã xếp hàng đợi.
+/seek hoặc /cseek - Chuyển tiếp Tìm kiếm nhạc theo thời lượng của bạn
+/seekback hoặc /cseekback - Lùi Tìm nhạc theo thời lượng của bạn
+/restart - Khởi động lại bot cho cuộc trò chuyện của bạn.
 
 
-HELP_2 = """✅<u>**Play Commands:**</u>
+✅<u>**Bỏ qua cụ thể:**</u>
+/skip hoặc /cskip [Số (ví dụ: 3)]
+    - Bỏ qua âm nhạc đến một số hàng đợi được chỉ định. Ví dụ: /skip 3 sẽ bỏ qua nhạc đến nhạc xếp hàng thứ ba và sẽ bỏ qua nhạc 1 và 2 trong hàng đợi.
 
-Available Commands = play , vplay , cplay
+✅<u>**Chơi vòng lặp:**</u>
+/loop hoặc /cloop [bật/tắt] hoặc [Các số từ 1-10]
+    - Khi được kích hoạt, bot sẽ lặp lại đoạn nhạc đang phát từ 1-10 lần khi trò chuyện bằng giọng nói. Mặc định là 10 lần.
 
-ForcePlay Commands = playforce , vplayforce , cplayforce
+✅<u>**Người dùng xác thực:**</u>
+Người dùng xác thực có thể sử dụng các lệnh quản trị mà không có quyền quản trị trong cuộc trò chuyện của bạn.
 
-**c** stands for channel play.
-**v** stands for video play.
-**force** stands for force play.
-
-/play or /vplay or /cplay  - Bot will start playing your given query on voice chat or Stream live links on voice chats.
-
-/playforce or /vplayforce or /cplayforce -  **Force Play** stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
-
-/channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
-
-
-✅**<u>Bot's Server Playlists:</u>**
-/playlist  - Check Your Saved Playlist On Servers.
-/deleteplaylist - Delete any saved music in your playlist
-/play  - Start playing Your Saved Playlist from Servers."""
+/auth [Tên người dùng] - Thêm người dùng vào DANH SÁCH AUTH của nhóm.
+/unauth [Tên người dùng] - Xóa người dùng khỏi DANH SÁCH AUTH của nhóm.
+/authusers - Kiểm tra DANH SÁCH AUTH của nhóm."""
 
 
-HELP_3 = """✅<u>**Bot Commands:**</u>
+HELP_2 = """✅<u>**Lệnh chơi:**</u>
 
-/stats - Get Top 10 Tracks Global Stats, Top 10 Users of bot, Top 10 Chats on bot, Top 10 Played in a chat etc etc.
+Các lệnh có sẵn = play , vplay , cplay
 
-/sudolist - Check Sudo Users of Yukki Music Bot
+Lệnh ForcePlay = playforce , vplayforce , cplayforce
 
-/lyrics [Music Name] - Searches Lyrics for the particular Music on web.
+**c** là viết tắt của phát kênh.
+**v** là viết tắt của phát video.
+**force** là viết tắt của chơi ép buộc.
 
-/song [Track Name] or [YT Link] - Download any track from youtube in mp3 or mp4 formats.
+/play hoặc /vplay hoặc /cplay - Bot sẽ bắt đầu phát truy vấn đã cho của bạn trong trò chuyện thoại hoặc Phát liên kết trực tiếp trên các cuộc trò chuyện thoại.
 
-/player -  Get a interactive Playing Panel.
+/playforce hoặc /vplayforce hoặc /cplayforce - **Force Play** dừng bản nhạc đang phát trên trò chuyện thoại và bắt đầu phát bản nhạc đã tìm kiếm ngay lập tức mà không làm phiền/xóa hàng đợi.
 
-**c** stands for channel play.
+/channelplay [Tên người dùng hoặc id trò chuyện] hoặc [Tắt] - Kết nối kênh với một nhóm và truyền phát nhạc trên cuộc trò chuyện thoại của kênh từ nhóm của bạn.
 
-/queue or /cqueue- Check Queue List of Music."""
+✅**<u>Danh sách phát trên máy chủ của Bot:</u>**
+/playlist - Kiểm tra danh sách phát đã lưu của bạn trên máy chủ.
+/deleteplaylist - Xóa mọi bản nhạc đã lưu trong danh sách phát của bạn
+/play - Bắt đầu phát Danh sách phát đã lưu của bạn từ Máy chủ."""
 
-HELP_4 = """✅<u>**Extra  Commands:**</u>
-/start - Start the Music Bot.
-/help  - Get Commands Helper Menu with detailed explanations of commands.
-/ping- Ping the Bot and check Ram, Cpu etc stats of Bot.
 
-✅<u>**Group Settings:**</u>
-/settings - Get a complete group's settings with inline buttons
+HELP_3 = """✅<u>**Lệnh của bot:**</u>
 
-🔗 **Options in Settings:**
+/stats - Nhận số liệu thống kê toàn cầu về 10 bản nhạc hàng đầu, 10 người dùng bot hàng đầu, 10 cuộc trò chuyện hàng đầu trên bot, 10 người chơi hàng đầu trong một cuộc trò chuyện, v.v.
 
-1️⃣ You can set **Audio Quality** you want to stream on voice chat.
+/sudolist - Kiểm tra Người dùng Sudo của Yukki Music Bot
 
-2️⃣ You can set **Video Quality** you want to stream on voice chat.
+/lyrics [Tên nhạc] - Tìm kiếm lời bài hát cho Nhạc cụ thể trên web.
 
-3️⃣ **Auth Users**:- You can change admin commands mode from here to everyone or admins only. If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
+/song [Tên bản nhạc] hoặc [YT Link] - Tải xuống bất kỳ bản nhạc nào từ youtube ở định dạng mp3 hoặc mp4.
 
-4️⃣ **Clean Mode:** When enabled deletes the bot's messages after 5 mins from your group to make sure your chat remains clean and good.
+/player - Nhận Bảng chơi tương tác.
 
-5️⃣ **Command Clean** : When activated, Bot will delete its executed commands (/play, /pause, /shuffle, /stop etc) immediately.
+**c** là viết tắt của phát kênh.
 
-6️⃣ **Play Settings:**
+/queue hoặc /cqueue- Kiểm tra danh sách nhạc trong hàng đợi."""
 
-/playmode - Get a complete play settings panel with buttons where you can set your group's play settings. 
+HELP_4 = """✅<u>**Lệnh bổ sung:**</u>
+/start - Bắt đầu Music Bot.
+/help - Nhận Menu Trình trợ giúp Lệnh với giải thích chi tiết về các lệnh.
+/ping- Ping Bot và kiểm tra chỉ số Ram, Cpu, v.v. của Bot.
 
-<u>Options in playmode:</u>
+✅<u>**Cài đặt nhóm:**</u>
+/settings - Nhận cài đặt của nhóm hoàn chỉnh bằng các nút nội tuyến
 
-1️⃣ **Search Mode** [Direct or Inline] - Changes your search mode while you give /play mode. 
+🔗 **Tùy chọn trong Cài đặt:**
 
-2️⃣ **Admin Commands** [Everyone or Admins] - If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
+1️⃣ Bạn có thể đặt **Chất lượng âm thanh** mà bạn muốn phát trên trò chuyện thoại.
 
-3️⃣ **Play Type** [Everyone or Admins] - If admins, only admins present in group can play music on voice chat."""
+2️⃣ Bạn có thể đặt **Chất lượng video** mà bạn muốn phát trên trò chuyện thoại.
 
-HELP_5 = """🔰**<u>ADD & REMOVE SUDO USERS :</u>**
-/addsudo [Username or Reply to a user]
-/delsudo [Username or Reply to a user]
+3️⃣ **Người dùng xác thực**: - Bạn có thể thay đổi chế độ lệnh của quản trị viên từ đây thành mọi người hoặc chỉ quản trị viên. Nếu mọi người, bất kỳ ai có mặt trong nhóm của bạn đều có thể sử dụng các lệnh của quản trị viên (như /skip, /stop, v.v.)
+
+4️⃣ **Chế độ sạch:** Khi được bật, bot sẽ xóa các tin nhắn khỏi nhóm của bạn sau 5 phút để đảm bảo cuộc trò chuyện của bạn vẫn sạch sẽ và tốt.
+
+5️⃣ **Command Clean** : Khi được kích hoạt, Bot sẽ xóa các lệnh đã thực thi của nó (/play, /pause, /shuffle, /stop, v.v.) ngay lập tức.
+
+6️⃣ **Cài đặt chơi:**
+
+/playmode - Nhận bảng cài đặt phát hoàn chỉnh với các nút nơi bạn có thể đặt cài đặt phát cho nhóm của mình.
+
+<u>Các tùy chọn trong chế độ phát:</u>
+
+1️⃣ **Chế độ tìm kiếm** [Trực tiếp hoặc Nội tuyến] - Thay đổi chế độ tìm kiếm của bạn trong khi bạn cung cấp /chế độ phát.
+
+2️⃣ **Lệnh quản trị viên** [Mọi người hoặc Quản trị viên] - Nếu mọi người, bất kỳ ai có mặt trong nhóm của bạn sẽ có thể sử dụng các lệnh quản trị viên (như /skip, /stop, v.v.)
+
+3️⃣ **Loại phát** [Mọi người hoặc Quản trị viên] - Nếu là quản trị viên, chỉ quản trị viên có mặt trong nhóm mới có thể phát nhạc trong trò chuyện thoại."""
+
+HELP_5 = """🔰**<u>THÊM VÀ XÓA NGƯỜI DÙNG SUDO :</u>**
+/addsudo [Tên người dùng hoặc Trả lời người dùng]
+/delsudo [Tên người dùng hoặc Trả lời người dùng]
 
 🛃**<u>HEROKU:</u>**
-/usage - Dyno Usage.
+/ cách sử dụng - Cách sử dụng Dyno.
 
-🌐**<u>CONFIG VARS:</u>**
-/get_var - Get a config var from Heroku or .env.
-/del_var - Delete any var on Heroku or .env.
-/set_var [Var Name] [Value] - Set a Var or Update a Var on heroku or .env. Seperate Var and its Value with a space.
+🌐**<u>CẤU HÌNH VARS:</u>**
+/get_var - Nhận một var cấu hình từ Heroku hoặc .env.
+/del_var - Xóa bất kỳ var nào trên Heroku hoặc .env.
+/set_var [Tên biến] [Giá trị] - Đặt biến hoặc cập nhật biến trên heroku hoặc .env. Tách Var và Giá trị của nó bằng một khoảng trắng.
 
-🤖**<u>BOT COMMANDS:</u>**
-/reboot - Reboot your Bot. 
-/update - Update Bot.
-/speedtest - Check server speeds
-/maintenance [enable / disable] 
-/logger [enable / disable] - Bot logs the searched queries in logger group.
-/get_log [Number of Lines] - Get log of your bot from heroku or vps. Works for both.
-/autoend [enable|disable] - Enable Auto stream end after 3 mins if no one is listening.
+🤖**<u>LỆNH BOT:</u>**
+/reboot - Khởi động lại Bot của bạn.
+/update - Cập nhật Bot.
+/speedtest - Kiểm tra tốc độ máy chủ
+/bảo trì [bật/tắt]
+/logger [bật/tắt] - Bot ghi lại các truy vấn đã tìm kiếm trong nhóm nhật ký.
+/get_log [Số dòng] - Nhận nhật ký bot của bạn từ heroku hoặc vps. Hoạt động cho cả hai.
+/autoend [bật|tắt] - Bật Tự động kết thúc luồng sau 3 phút nếu không có ai nghe.
 
-📈**<u>STATS COMMANDS:</u>**
-/activevoice - Check active voice chats on bot.
-/activevideo - Check active video calls on bot.
-/stats - Check Bots Stats
+📈**<u>LỆNH THỐNG KÊ:</u>**
+/activevoice - Kiểm tra các cuộc trò chuyện bằng giọng nói đang hoạt động trên bot.
+/activevideo - Kiểm tra các cuộc gọi video đang hoạt động trên bot.
+/stats - Kiểm tra số liệu thống kê bot
 
-⚠️**<u>BLACKLIST CHAT FUNCTION:</u>**
-/blacklistchat [CHAT_ID] - Blacklist any chat from using Music Bot
-/whitelistchat [CHAT_ID] - Whitelist any blacklisted chat from using Music Bot
-/blacklistedchat - Check all blacklisted chats.
+⚠️**<u>CHỨC NĂNG TRÒ CHUYỆN TRONG DANH SÁCH ĐEN:</u>**
+/blacklistchat [CHAT_ID] - Đưa vào danh sách đen mọi cuộc trò chuyện sử dụng Music Bot
+/whitelistchat [CHAT_ID] - Đưa bất kỳ cuộc trò chuyện nào vào danh sách đen vào danh sách cấm sử dụng Music Bot
+/blacklistedchat - Kiểm tra tất cả các cuộc trò chuyện trong danh sách đen.
 
-👤**<u>BLOCKED FUNCTION:</u>**
-/block [Username or Reply to a user] - Prevents a user from using bot commands.
-/unblock [Username or Reply to a user] - Remove a user from Bot's Blocked List.
-/blockedusers - Check blocked Users Lists
+👤**<u>CHỨC NĂNG BỊ CHẶN:</u>**
+/block [Tên người dùng hoặc Trả lời người dùng] - Ngăn người dùng sử dụng lệnh bot.
+/unblock [Tên người dùng hoặc Trả lời người dùng] - Xóa người dùng khỏi Danh sách chặn của Bot.
+/blockedusers - Kiểm tra danh sách người dùng bị chặn
 
-👤**<u>GBAN FUNCTION:</u>**
-/gban [Username or Reply to a user] - Gban a user from bot's served chat and stop him from using your bot.
-/ungban [Username or Reply to a user] - Remove a user from Bot's gbanned List and allow him for using your bot
-/gbannedusers - Check Gbanned Users Lists
+👤**<u>CHỨC NĂNG GBAN:</u>**
+/gban [Tên người dùng hoặc Trả lời người dùng] - Gban người dùng khỏi cuộc trò chuyện do bot cung cấp và ngăn anh ta sử dụng bot của bạn.
+/ungban [Tên người dùng hoặc Trả lời người dùng] - Xóa người dùng khỏi Danh sách bị cấm của Bot và cho phép anh ta sử dụng bot của bạn
+/gbannedusers - Kiểm tra danh sách người dùng bị Gbanned
 
-🎥**<u>VIDEOCALLS FUNCTION:</u>**
-/set_video_limit [Number of Chats] - Set a maximum Number of Chats allowed for Video Calls at a time. Default to 3 chats.
-/videomode [download|m3u8] - If download mode is enabled, Bot will download videos instead of playing them in M3u8 form. ByDefault to M3u8. You can use download mode when any query doesnt plays in m3u8 mode.
+🎥**<u>CHỨC NĂNG CUỘC GỌI VIDEO:</u>**
+/set_video_limit [Số lượng Trò chuyện] - Đặt Số lượng Trò chuyện tối đa được phép cho Cuộc gọi video tại một thời điểm. Mặc định là 3 cuộc trò chuyện.
+/videomode [download|m3u8] - Nếu bật chế độ tải xuống, Bot sẽ tải xuống video thay vì phát chúng ở dạng M3u8. Theo Mặc định cho M3u8. Bạn có thể sử dụng chế độ tải xuống khi bất kỳ truy vấn nào không phát ở chế độ m3u8.
 
-⚡️**<u>PRIVATE BOT FUNCTION:</u>**
-/authorize [CHAT_ID] - Allow a chat for using your bot.
-/unauthorize [CHAT_ID] - Disallow a chat from using your bot.
-/authorized - Check all allowed chats of your bot.
+⚡️**<u>CHỨC NĂNG BOT RIÊNG TƯ:</u>**
+/ủy quyền [CHAT_ID] - Cho phép trò chuyện để sử dụng bot của bạn.
+/unauthorize [CHAT_ID] - Không cho phép trò chuyện sử dụng bot của bạn.
+/ ủy quyền - Kiểm tra tất cả các cuộc trò chuyện được phép của bot của bạn.
 
-🌐**<u>BROADCAST FUNCTION:</u>**
-/broadcast [Message or Reply to a Message] - Broadcast any message to Bot's Served Chats.
+🌐**<u>CHỨC NĂNG PHÁT SÓNG:</u>**
+/broadcast [Tin nhắn hoặc Trả lời tin nhắn] - Phát bất kỳ tin nhắn nào tới các cuộc trò chuyện được phân phát của Bot.
 
-<u>options for broadcast:</u>
-**-pin** : This will pin your message 
-**-pinloud** : This will pin your message with loud notification
-**-user** : This will broadcast your message to the users who have started your bot.
-**-assistant** : This will broadcast your message from assistant account of your bot.
-**-nobot** : This will force your bot to not broadcast message
+<u>các tùy chọn phát sóng:</u>
+**-pin** : Thao tác này sẽ ghim tin nhắn của bạn
+**-pinloud** : Điều này sẽ ghim tin nhắn của bạn với thông báo lớn
+**-user** : Thao tác này sẽ phát tin nhắn của bạn tới những người dùng đã khởi động bot của bạn.
+**-assistant** : Thao tác này sẽ phát tin nhắn của bạn từ tài khoản trợ lý của bot.
+**-nobot** : Điều này sẽ buộc bot của bạn không phát tin nhắn
 
-**Example:** `/broadcast -user -assistant -pin Hello Testing`
+**Ví dụ:** `/broadcast -user -assistant -pin Thử nghiệm xin chào`
 
 """
